c0d4ab8cca330a15f8dbfe09d08fe9a4ec05f16c
june.kim <junho85@gmail.com>
2021-11-23 00:39:31 +0900

    수동 등록 데이터

--- attendance/manual_insert.py
+++ attendance/manual_insert.py
@@ -1,16 +1,19 @@
 from datetime import datetime
 
 from garden import Garden
+
+from attendance.mongo_tools import MongoTools
+
 import pprint
 import requests
 
 from urllib.parse import urlparse
 
+mongo_tools = MongoTools()
+
 garden = Garden()
 
-db = garden.get_database()
-
-mongo_collection = db.get_collection("slack_messages")
+mongo_collection = mongo_tools.get_collection()
 
 def get_commit(commit_url):
     parse_result = urlparse(commit_url)
@@ -21,9 +24,12 @@ def get_commit(commit_url):
 
     r = requests.get(api_url)
 
+    # pprint(r)
     # print(r.json())
-    ts_datetime = datetime.strptime(r.json()["commit"]["author"]["date"], "%Y-%m-%dT%H:%M:%S%z")
-    message = r.json()["commit"]["message"]
+    commit_json = r.json()
+
+    ts_datetime = datetime.strptime(commit_json["commit"]["author"]["date"], "%Y-%m-%dT%H:%M:%S%z")
+    commit_message = commit_json["commit"]["message"]
     ts = str(ts_datetime.timestamp())
     return {
         "user": user,
@@ -31,46 +37,94 @@ def get_commit(commit_url):
         "ts_datetime": ts_datetime,
         "sha": sha,
         "sha_short": sha[:8],
-        "message": message,
+        "message": commit_message,
         }
 
-# commit_url = 'https://github.com/gintire/TIL/commit/f7c915de5e029270744553ca2b0f88da823e44c5'
-#commit_url = 'https://github.com/gintire/TIL/commit/93a4f58de89830b9cca0f1d554c2fe3656531bc3'
-#commit_url = 'https://github.com/js7483/ddd-study/commit/94e153a2ffa1768c1843f414375616d73c68b93a'
-# commit_url = 'https://github.com/Wealgo/changmin/commit/d6ecda2127ee11d2741bb42258affaf2e803bcb0'
-commit_url = 'https://github.com/itsnamgyu/TIW/commit/f3d807901dc16d1c049dc14c02c1916234af8abc'
 
-commit = get_commit(commit_url)
-# print(commit)
-print(commit["user"])
-print(commit["sha"])
-print(commit["sha_short"])
-print(commit["ts"])
-print(commit["ts_datetime"])
-print(commit["ts_datetime"].strftime("%Y-%m-%d"))
-print(commit["message"])
+def manual_insert(commit_url):
+    commit = get_commit(commit_url)
+    print("commit:")
+    print(commit)
+    print("user:" + commit["user"])
+    print("sha:" + commit["sha"])
+    print("sha_short:" + commit["sha_short"])
+    print("ts:" + commit["ts"])
+    print("ts_datetime:" + str(commit["ts_datetime"]))
+    print("ts_datetime %Y-%m-%d:" + commit["ts_datetime"].strftime("%Y-%m-%d"))
+    print("message:" + commit["message"])
 
-text = '*manual insert %s by june.kim*\n<%s|`%s`> - %s' % ("2020-04-11", commit_url, commit["sha_short"], commit["message"])
+    today = datetime.today().strftime("%Y-%m-%d")
+    fallback = '*manual insert %s by june.kim* - commit by %s' % (today, commit["user"])
+    text = '`<%s|%s>` - %s' % (commit_url, commit["sha_short"], commit["message"])
 
-message = {'attachments':
-    [{
+    user = 'U02LXDBJZRV'
+    message = {'attachments':
+        [{
+            'fallback': fallback,
+            'text': text
+        }],
         'author_name': commit["user"],
-        'text': text
-    }],
-    'ts': commit["ts"],
-    'ts_for_db': commit["ts_datetime"],
-    'type': 'message',
-    'user': 'UU9RDT66M'
-}
+        'ts': commit["ts"],
+        'ts_for_db': commit["ts_datetime"],
+        'type': 'message',
+        'user': user
+    }
 
-print(message)
-# exit(-1)
+    pprint.pprint(message)
+    # exit(-1)
 
+    try:
+        result = mongo_collection.insert_one(message)
+        pprint.pprint(result)
+        print(message)
+    except Exception as e:
+        print(e)
 
-try:
-    result = mongo_collection.insert_one(message)
-    pprint.pprint(result)
-    print(message)
-except Exception as e:
-    print(e)
+commit_urls = [
+    # 오스카
+    'https://github.com/lunaticfoxy/TIL/commit/295f8bfa02a1d65794eacb95dec3ca05d08bd7bb', # 11.15
+    # genie-youn
+    'https://github.com/genie-youn/TIL/commit/c3e59e5dfdf02347808e4cf167324b33970c08ef', # 11.15
+    # 클로이
+    'https://github.com/chloeeekim/TIL/commit/567d40daf425dc79365835b9196789c510084392',  # 11.15
+    # 최혜원
+    'https://github.com/xizsmin/TIL/commit/15e8bd79dfd821f2344e4dbd59f6af569fcc65b6',  # 11.15
+    # 샐리
+    'https://github.com/sally-yeom/TIL/commit/0780869aa43301d8d5627b28623f6e26fc82393c',  # 11.15
+    # 김희라_sage
+    'https://github.com/lallaheeee/TIL/commit/806efe4cc6d6b5730ea3f5abb417b682bea0423c',  # 11.15
+    # 강지혜
+    'https://github.com/wwisdom46/TIL/commit/e35e4705abd639c51290e2e2457f43571ada5aa6',  # 11.15
+    # 김규태
+    'https://github.com/gyutae100/TIL/commit/31081cf844ec792ab8227942ef4eace5de9d0250',  # 11.15
+    # InseobJeon
+    'https://github.com/InseobJeon/the-gardeners-7th-inseob/commit/33c894daa863e1d57cec19c0bf4dad565a84ad60',
+    # junho85
+    # jiyeoon
+    'https://github.com/jiyeoon/TIL/commit/32f605c8e46fb207fc9299846c9fd532520f983a', # 11.15
+    # 노찬우
+    'https://github.com/rajephon/TIL/commit/58f3275cd1c2f13ff3a42c1d720c7996d750ee3b',  # 11.15
+    # 미카
+    'https://github.com/lumiamitie/TIL/commit/76d2d1bd716fde1851ddd96f5505e5f4af5d0ee3', # 11.15
+    'https://github.com/lumiamitie/TIL/commit/c6169c3a7f1121e4d51815727cc8c8e51327ccd0', # 11.15
+    # 최준우
+    'https://github.com/mike6321/TIL/commit/53dee824e2a87c6131d6fbca813593096bfcbaf5', # 11.15
+    # 지나
+    'https://github.com/koyrkr/TIL/commit/47c2f18728af3e0cc6935d079ad2383e6f388cd7',  # 11.15 - 11.16 00:57
+    # 김도담
+    'https://github.com/dodam-kim/TIL/commit/d833e75e67485d0438ce0c84f3bdf4f47e99b9c2',  # 11.15
+    # codingbowoo
+    # 고언약
+    'https://github.com/brave-people/Dev-Event/commit/3e4fe876c29e9cd386c06f604cdcce7aec1dcef6', # 11.15 repo가 다르지만 처음이라 인정. author_name 수작업 필요
+    # estellechoi
+    # DS-DavGu
+    # bloomspes
+    'https://github.com/bloomspes/hackerrank/commit/82ea28ab73e37c662668c151e436c215a522332e', # 22
+    'https://github.com/bloomspes/hackerrank/commit/62bc8fd3751615751fea7ad443e87f547ff11bae', # 22
+    'https://github.com/bloomspes/hackerrank/commit/543a397898113f7066c89ee43a142f5c7ee45cf7', # 18
+    'https://github.com/bloomspes/hackerrank/commit/74f709fe8f9eef052a8436ec8c678d9d67664fa5', # 18
+    # heejour
+]
 
+for commit_url in commit_urls:
+    manual_insert(commit_url)
